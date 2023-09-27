807354f4b4f4d8fb9df7240a6329505154352a7f
AksWorld <123278691+AksWorld@users.noreply.github.com>
2023-09-26 19:40:57 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function preload(){
+}
+function setup(){
+    Canvas=createCanvas(300,300);
+    Canvas.center();
+}
+function draw(){
+
+}
+function Operation_Camera_Man(){
+    save("Camera_man_filters.png");}
